79aac3a6e070811430bee5df10ed6d0e47faef83
p00ria <amir1300sobatdar@gmail.com>
2016-03-20 14:49:38 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -361,102 +361,108 @@ Commands list :
 
 !kick [username|id]
 You can also do it by reply
-
+〰〰〰〰〰〰〰
 !ban [ username|id]
 You can also do it by reply
-
+〰〰〰〰〰〰〰
 !unban [id]
 You can also do it by reply
-
+〰〰〰〰〰〰〰
 !who
 Members list
-
+〰〰〰〰〰〰〰
 !modlist
 Moderators list
-
+〰〰〰〰〰〰〰
 !promote [username]
 Promote someone
-
+〰〰〰〰〰〰〰
 !demote [username]
 Demote someone
-
+〰〰〰〰〰〰〰
 !kickme
 Will kick user
-
+〰〰〰〰〰〰〰
 !about
 Group description
-
+〰〰〰〰〰〰〰
 !setphoto
 Set and locks group photo
-
+〰〰〰〰〰〰〰
 !setname [name]
 Set group name
-
+〰〰〰〰〰〰〰
 !rules
 Group rules
-
+〰〰〰〰〰〰〰
 !id
 Return group id or user id
-
+〰〰〰〰〰〰〰
 !help
 Get commands list
-
+〰〰〰〰〰〰〰
 !lock [member|name|bots|ads|tag|join|arabic|leave] 
 Locks [member|name|bots|ads|tag|join|arabic|leaveing] 
-
+〰〰〰〰〰〰〰
 !unlock [member|name|bots|ads|tag|join|arabic|leave]
 Unlocks [member|name|bots|ads|tag|join|arabic|leaving]
-
+〰〰〰〰〰〰〰
 !set rules [text]
 Set [text] as rules
-
+〰〰〰〰〰〰〰
 !set about [text]
 Set [text] as about
-
+〰〰〰〰〰〰〰
 !settings
 Returns group settings
-
+〰〰〰〰〰〰〰
 !newlink
 Create/revoke your group link
-
+〰〰〰〰〰〰〰
 !link
 Returns group link
-
+〰〰〰〰〰〰〰
 !owner
 Returns group owner id
-
+〰〰〰〰〰〰〰
 !setowner [id]
 Will set id as owner
-
+〰〰〰〰〰〰〰
 !setflood [value]
 Set [value] as flood sensitivity
-
+〰〰〰〰〰〰〰
+!map [country|province|city]
+Returns map and location from [country|province|city]
+〰〰〰〰〰〰〰
+!info
+Returns user information
+〰〰〰〰〰〰〰
 !stats
 Simple message statistics
-
+〰〰〰〰〰〰〰
 !save [value] [text]
 Save [text] as [value]
-
+〰〰〰〰〰〰〰
 !get [value]
 Returns text of [value]
-
+〰〰〰〰〰〰〰
 !clean [modlist|rules|about]
 Will clear [modlist|rules|about] and set it to nil
-
+〰〰〰〰〰〰〰
 !res [username]
 Returns user id
-
+〰〰〰〰〰〰〰
 !log
 Will return group logs
-
+〰〰〰〰〰〰〰
 !banlist
 Will return group ban list
-
+〰〰〰〰〰〰〰
 » U can use both "/" and "!" 
 
 » Only mods, owner and admin can add bots in group
 
-» Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+» Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,map,setname,lock,unlock,set rules,set about and settings commands
 
 » Only owner can use res,setowner,promote,demote and log commands
 
